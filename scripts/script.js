a0508797745fcfbe05fb3d1145d67364c9067037
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 17:20:48 -0500

    donut forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
+ // onclick="$('#plainglaze').show()"
+ // onclick="$('#chocosprinkle').hide()"
 
+$(document).ready(function() {
+   $('#submit').change(function() {
+       $('.flavor').hide();
+       $('.' + $(this).val()).show();
+   });
+});
